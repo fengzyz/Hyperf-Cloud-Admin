e8ffd3361b992ff09d34e88e4bc36780697450c9
shuyh <nelson.shu.chn@gmail.com>
2019-12-27 10:34:21 +0800

    封装RPC组件

---
+++ service-components/src/Enum/StatusEnum.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fengzyz
+ * Date: 2019/12/20
+ * Time: 22:45
+ */
+
+namespace ServiceComponents\Enum;
+
+
+class StatusEnum
+{
+    const FailCode = 1001;//失败码
+    const SuccessCode = 1000;//成功码
+    const Fail = 4;
+    const Success = 1;
+}
---
+++ service-components/src/Enum/UserEnum.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/12/22
+ * Time: 上午 9:24
+ */
+
+namespace ServiceComponents\Enum;
+
+
+class UserEnum
+{
+    const Friend = 'friend';
+    const Group = 'group';
+}
